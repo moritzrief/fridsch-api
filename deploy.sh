707a25367f850d2ae30982198bb37fec6d107e8c
Moritz Rief <moritz.rief@gmail.com>
2025-01-19 15:55:13 +0100

    :alien: Update URL

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #rm package-lock.json
 #rm -rf node_modules
 #scp -r ./ ubuntu@mrief.tech:./fridsch_api
-rsync -rav -e ssh --exclude='node_modules' ./ ubuntu@mrief.tech:./fridsch_api
-ssh ubuntu@mrief.tech 'bash -s' <<'ENDSSH'
+rsync -rav -e ssh --exclude='node_modules' ./ ubuntu@mrief.xyz:./fridsch_api
+ssh ubuntu@mrief.xyz 'bash -s' <<'ENDSSH'
     cd fridsch_api
     sudo docker stop fridsch
     sudo docker rm fridsch
     sudo docker build . -t fridsch-api
-    sudo docker run -d -p 80:3333 --name fridsch fridsch-api
+    sudo docker run -d -p 8083:3333 --name fridsch fridsch-api
 ENDSSH
 
 echo 'all commands executed'
